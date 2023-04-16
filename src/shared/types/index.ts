7f4e20310dfc6b0847e3405a3fc78002ac610592
fliseno1k <fliseno1k@gmail.com>
2023-04-17 06:07:06 +1000

    init: create empty next app

--- src/shared/types/index.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react';
-
-export type As<Props = any> = ElementType<Props>;
-
-export type PropsOf<T extends As> = ComponentPropsWithoutRef<T> & {
-	as?: As;
-};
