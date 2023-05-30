6bfc1bbd08c96bc42ee3162ba837b16a2acf64d4
fliseno1k <fliseno1k@gmail.com>
2023-05-30 22:36:58 +1000

    add footer meta

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -11,8 +11,12 @@ export const Footer: FC<FooterProps> = props => {
 	return (
 		<footer className={s._}>
 			<Container className={s.inner}>
-				<div className={s.socials}></div>
-				<div className={s.following}></div>
+				<div className={s.column}>
+					<span className={s.company}>BSC METAVERSE LIMITED</span>
+					<span className={s.address}>Suite 305, Griffith Corporate Centre</span>
+					<span className={s.mailing}>P.O. Box 1510, Beachmont Kingstown St. Vincent and the Grenadines</span>
+					<span className={s.number}>26818 BC 2022</span>
+				</div>
 			</Container>
 		</footer>
 	);
